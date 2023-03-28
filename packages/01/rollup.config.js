448688ef9b8d32a10c862125000d5dbb63dff5fa
weiyafei <2553241022@qq.com>
2023-03-28 14:41:42 +0800

    多产物配置

--- packages/01/rollup.config.js
+++ packages/01/rollup.config.js
@@ -2,23 +2,26 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-03-27 13:05:27
- * @LastEditTime: 2023-03-28 13:12:32
+ * @LastEditTime: 2023-03-28 14:40:09
  * @LastEditors: wsy
  */
 const path = require('node:path')
 const typescript = require('rollup-plugin-typescript2')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
-const resolvePath = dir => path.resolve(__dirname, dir)
+const { defineConfig } = require('rollup')
 
-const buildOptions = {
-  input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
+module.exports = defineConfig({
+  input: ['src/index.ts', 'src/util/util.ts'],
+  output: [{
+    dir: 'dist/es',
     format: 'esm',
   },
+  {
+    dir: 'dist/cjs',
+    format: 'cjs',
+  }],
   plugins: [
     nodeResolve(),
     typescript(),
   ],
-}
-module.exports = buildOptions
+})
