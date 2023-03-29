12af73ed096d50cdf992a86372b59de3cff2aad5
weiyafei <2553241022@qq.com>
2023-03-29 14:50:05 +0800

    plugins and external

--- packages/01/rollup.config.js
+++ packages/01/rollup.config.js
@@ -2,26 +2,33 @@
  * @Description:
  * @Author: wsy
  * @Date: 2023-03-27 13:05:27
- * @LastEditTime: 2023-03-28 14:40:09
+ * @LastEditTime: 2023-03-29 14:45:34
  * @LastEditors: wsy
  */
 const path = require('node:path')
 const typescript = require('rollup-plugin-typescript2')
 const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
 const { defineConfig } = require('rollup')
 
 module.exports = defineConfig({
-  input: ['src/index.ts', 'src/util/util.ts'],
-  output: [{
-    dir: 'dist/es',
-    format: 'esm',
-  },
-  {
-    dir: 'dist/cjs',
-    format: 'cjs',
-  }],
+  input: ['src/index.ts'],
+  output: [
+    {
+      dir: 'dist/es',
+      entryFileNames: '[name]-1.js',
+      chunkFileNames: 'chunk-[hash].js',
+      format: 'esm',
+    },
+    {
+      dir: 'dist/cjs',
+      format: 'cjs',
+    },
+  ],
   plugins: [
     nodeResolve(),
+    commonjs(),
     typescript(),
   ],
+  external: ['lodash'],
 })
