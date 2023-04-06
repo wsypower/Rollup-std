ffa0c94705f01e74fbdde1d4810755e7a8852fd6
weiyafei <2553241022@qq.com>
2023-04-06 12:39:12 +0800

    调整eslint

---
+++ packages/basic/src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-03-28 13:00:01
+ * @LastEditTime: 2023-03-29 14:43:49
+ * @LastEditors: wsy
+ */
+import { get } from 'lodash'
+import { add } from './util/util'
+// eslint-disable-next-line no-console
+console.log(add(1, 2))
+get({ a: 1 }, 'a')
