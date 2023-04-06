290a7ebfa620be5179159a5785d5d0e822fa8156
weiyafei <2553241022@qq.com>
2023-04-06 12:39:38 +0800

    调整eslint

--- packages/magic-string/index.ts
+++ packages/magic-string/index.ts
@@ -1,9 +1,50 @@
-const fs = require('node:fs')
+/*
+ * @Description:
+ * @Author: wsy
+ * @Date: 2023-03-31 14:18:32
+ * @LastEditTime: 2023-04-03 12:47:16
+ * @LastEditors: wsy
+ */
 const MagicString = require('magic-string')
 
-const code = 'console.log("Hello, world!");'
-const magicString = new MagicString(code)
+const bundle = new MagicString.Bundle()
 
-magicString.insertRight(code.length - 2, '----------------------')
+bundle.addSource({
+  filename: 'foo.js',
+  content: new MagicString('var answer = 42;'),
+})
 
-console.log(magicString.toString())
+bundle.addSource({
+  filename: 'bar.js',
+  content: new MagicString('console.log( answer )'),
+})
+
+// Sources can be marked as ignore-listed, which provides a hint to debuggers
+// to not step into this code and also don't show the source files depending
+// on user preferences.
+bundle.addSource({
+  filename: 'some-3rdparty-library.js',
+  content: new MagicString('function myLib(){}'),
+  ignoreList: false, // <--
+})
+
+// Advanced: a source can include an `indentExclusionRanges` property
+// alongside `filename` and `content`. This will be passed to `s.indent()`
+// - see documentation above
+
+bundle.indent() // optionally, pass an indent string, otherwise it will be guessed
+  .prepend('(function () {\n')
+  .append('}());')
+
+bundle.toString()
+// (function () {
+//   var answer = 42;
+//   console.log( answer );
+// }());
+
+// options are as per `s.generateMap()` above
+const map = bundle.generateMap({
+  file: 'bundle.js',
+  includeContent: true,
+  hires: true,
+})
